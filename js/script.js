81d59f5d7f21d581de8f818c6e179756e6be840c
Michał Szczepanik <michal.szczepanik@gmail.com>
2021-05-10 23:20:37 +0200

    transformed js functions into arrow functions

--- js/script.js
+++ js/script.js
@@ -1,15 +1,35 @@
-console.log("Hello!");
+{
+    const welcome = () => console.log("Hello!");
 
-let mainSection = document.querySelector(".main__section");
-let bodyButtonShowHide = document.querySelector(".js-body__buttonShowHide");
-let bodyButtoRemove = document.querySelector(".js-body__buttonRemove");
-let mainButton = document.querySelector(".js-main__button")
-let mainImg = document.querySelector(".js-main__img")
-let picOnePositive = "https://i.ibb.co/4FqZ8pc/111.jpg"
-let picOneNegative = "https://i.ibb.co/jkNK4Ch/1.jpg"
+    const changeImage = () => {
+        const mainButton = document.querySelector(".js-main__button")
+        const mainImg = document.querySelector(".js-main__img")
+        const picOnePositive = "https://i.ibb.co/4FqZ8pc/111.jpg"
+        const picOneNegative = "https://i.ibb.co/jkNK4Ch/1.jpg"
 
-mainButton.addEventListener("click", () => { mainImg.src = mainImg.src === picOnePositive ? picOneNegative : picOnePositive; })
+        mainButton.addEventListener("click", () => { mainImg.src = mainImg.src === picOnePositive ? picOneNegative : picOnePositive; })
+    }
 
-bodyButtonShowHide.addEventListener("click", () => { mainSection.classList.toggle("main__section--showNone"); });
+    const showHideMainSection = (mainSection) => {
+        const bodyButtonShowHide = document.querySelector(".js-body__buttonShowHide");
 
-bodyButtoRemove.addEventListener("click", () => { mainSection.remove(); });
+        bodyButtonShowHide.addEventListener("click", () => { mainSection.classList.toggle("main__section--showNone"); });
+    }
+
+    const removeMainSection = (mainSection) => {
+        const bodyButtoRemove = document.querySelector(".js-body__buttonRemove");
+
+        bodyButtoRemove.addEventListener("click", () => { mainSection.remove(); });
+    }
+
+    const init = () => {
+        const mainSection = document.querySelector(".main__section");
+
+        welcome();
+        changeImage();
+        showHideMainSection(mainSection);
+        removeMainSection(mainSection);
+    }
+
+    init();
+}
